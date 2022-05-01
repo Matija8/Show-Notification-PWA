0651447a8855e28f6c1756fd9cb69f868402f086
matija8 <matijanme@gmail.com>
2022-05-01 23:22:44 +0200

    Updates

---
+++ _next/static/hXry7_Wz7AGg-cLOWb6jT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b2f69e06b8e07980.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
