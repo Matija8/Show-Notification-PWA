7b7fc9849eefca4e95492b94afd6621f3afb820f
matija8 <matijanme@gmail.com>
2022-05-01 22:37:30 +0200

    fix: Remove service worker

--- next.config.js
+++ next.config.js
@@ -1,23 +1,12 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
 
 const isDevEnv = process.env.NODE_ENV === 'development';
 const basePath = isDevEnv ? '' : '/Show-Notification-PWA';
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    runtimeCaching,
-    customWorkerDir: 'service-worker',
-  },
+module.exports = {
   reactStrictMode: true,
   env: {
     basePath,
   },
   basePath,
-  // i18n: {
-  //   locales: ['en'],
-  //   defaultLocale: 'en',
-  // },
-});
+};
